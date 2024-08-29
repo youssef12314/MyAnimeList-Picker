7d76eb6f771ea4defb1909efeb03617244a3c5ee
Youssef Talbi <112815657+youssef12314@users.noreply.github.com>
2024-08-29 15:37:13 +0200

    link takes you to the myanimelist site

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def fetch_plan_to_watch(username):
     url = f'https://api.myanimelist.net/v2/users/{username}/animelist'
     params = {
         'status': 'plan_to_watch',
-        'limit': 1000,  # Use a smaller limit for debugging
+        'limit': 1000,
         'fields': 'title,main_picture'
     }
     headers = {
@@ -19,16 +19,15 @@ def fetch_plan_to_watch(username):
 
     try:
         response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        print("API Response Status:", response.status_code)  # Debugging
+        response.raise_for_status()
         response_data = response.json()
-        print("API Response JSON:", response_data)  # Debugging
 
         anime_list = response_data.get('data', [])
         anime_info = [
             {
                 'title': anime['node']['title'],
-                'image_url': anime['node'].get('main_picture', {}).get('large', '')
+                'image_url': anime['node'].get('main_picture', {}).get('large', ''),
+                'url': f"https://myanimelist.net/anime/{anime['node']['id']}"  
             }
             for anime in anime_list
         ]
@@ -44,19 +43,17 @@ def index():
 @app.route('/get-random-anime', methods=['POST'])
 def get_random_anime():
     username = request.form.get('username')
-    print("Received Username:", username)  # Debugging
     anime_info_list = fetch_plan_to_watch(username)
     if anime_info_list:
         random_anime_info = random.choice(anime_info_list)
-        print("Random Anime Info:", random_anime_info)  # Debugging
         return render_template(
             'index.html',
             random_anime_title=random_anime_info['title'],
             random_anime_image_url=random_anime_info['image_url'],
+            random_anime_url=random_anime_info['url'],
             username=username
         )
     else:
-        print("No anime found or API call failed.")  # Debugging
         return render_template(
             'index.html',
             error='No anime found in your plan to watch list',
